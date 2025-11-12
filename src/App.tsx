8203907b08d22ca55704f3264b064ba8521868ac
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-11-11 23:13:49 -0300

    estilizacao da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+// import React from 'react';
+import './App.css';
+
+import logoImg from './assets/logo-oficina1.png';
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>teste</h1>
-      </div>
-    </>
+    <div>
+      <main className="container">
+        <img className="logo" src={logoImg} alt="logo da oficina do toty" />
+        <h1 className="title">Faça seu orçamento aqui!</h1>
+
+        <form className="form">
+          <label>Altura da grade (em cm)</label>
+          <input className="input" placeholder="100" />
+
+          <label>Largura da grade (em cm)</label>
+          <input className="input" placeholder="100 " />
+
+          <input className="button" type="submit" value="Calcular" />
+        </form>
+
+        <section className="result">
+          <h2 className="result-title">O valor da sua grade é:</h2>
+          <span>Metalon: </span>
+          <span>Barrinha:</span>
+        </section>
+      </main>
+    </div>
   );
 }
 
